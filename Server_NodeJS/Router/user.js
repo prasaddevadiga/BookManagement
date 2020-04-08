e5989e0c3495e4288d81dceacc09cb0e787402bc
Prasad Devadiga <prasad>
2020-04-08 12:34:34 +0530

    Implemented login API

--- Server_NodeJS/Router/user.js
+++ Server_NodeJS/Router/user.js
@@ -6,25 +6,37 @@ var Cryptr = require('cryptr');             // this is used to encrypt the passw
 const myConnection = require("../connection");
 
 Router.post('/register', (req, res) => {
-    var today = new Date()
-    var users = {
-        "name" : req.body.name,
-        "email": req.body.email,
-        "password": req.body.password,
-        "created_at": today,
-        "updated_at": today
-    }
-    myConnection.query("INSERT INTO users SET ?", users, (err, rows, fields) => {
-        if (err) {
-            res.json({"status": false, "message": "There are some error with query"})
+    var users = { "name" : req.body.name, "email": req.body.email, "password": req.body.password, "created_at": Date(), "updated_at": Date() }
+    myConnection.query("SELECT * from users WHERE email = ? ", req.body.email,  (err, results, fields) => {
+        if (err) throw err;
+        if (results.length > 0) {
+            res.json({ "status" : false, "message": req.body.email + " already in use" })
             return
         }
-        res.json({"status": true, "message": "User registered successfully"})
+
+        myConnection.query("INSERT INTO users SET ?", users, (err, rows, fields) => {
+            if (err) {
+                res.json({"status": false, "message": "There are some error with query"})
+                return
+            }
+            res.json({"status": true, "message": "User registered successfully"})
+        })
     })
 });
 
 Router.post('/login', (req, res) => {
-
+    myConnection.query("SELECT * from users WHERE email = ? ", [req.body.email],  (err, results, fields) => {
+        if (err) throw err;
+        if (results.length == 0) {
+            res.json({ "status" : false, "message": "User not registered" })
+            return
+        }
+        if (results[0].password != req.body.password) {
+            res.json({ "status" : false, "message": "Password entered is incorrect" })
+            return
+        }
+        res.json({ "status" : true, "message": "Successfully logged in" })
+    });
 });
 
 module.exports = Router;
